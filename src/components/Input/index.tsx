267015073ac93a8b1ca210913aebbae71fcc19a8
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-02-08 16:45:24 -0300

    feat: add simple files to input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Text, View} from "react-native"
+import styles from "./styles";
+
+function Input(){
+    return(
+        <View>
+            <Text>Lorem ipsum dolor sit amet</Text>
+        </View>
+    );
+}
+
+export default Input;
