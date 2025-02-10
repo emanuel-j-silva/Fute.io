37bf694a94e4c05e2650ca4678291535cdb0dea3
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-02-10 19:17:26 -0300

    feat: create simple files to Home screen

---
+++ src/screens/Home/styles.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    background:{
+        minWidth: "100%",
+        minHeight: "100%",
+        backgroundColor: "black"
+    },
+    overlay:{
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    text:{
+        textAlign: "center",
+        fontSize: 52,
+        color: "white",
+        fontFamily: "FasterOne"
+    }
+});
+
+export default styles;
