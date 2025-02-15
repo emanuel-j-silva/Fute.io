c3a8fc70b2d4d4f6409f2df8885d22ebc258517e
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-02-15 13:32:41 -0300

    feat: improve a simple impl for profile picture component

--- src/components/ProfilePicture/index.tsx
+++ src/components/ProfilePicture/index.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-import {Text, View} from "react-native"
+import { View, Image } from "react-native"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
 import styles from "./styles";
 
-function ProfilePicture(){
+interface ProfilePictureProps{
+    photoUrl?: string;
+    size?: number;
+}
 
+function ProfilePicture({photoUrl, size=26}: ProfilePictureProps){
+    if(photoUrl){
+            <Image 
+                source={{uri: photoUrl}}
+                style={[styles.image, {width: size, height: size, borderRadius: size / 2}]}
+            />
+    }else{
+        return(
+            <View style={styles.iconContainer}>
+                <MaterialCommunityIcons
+                    name={"account" as keyof typeof MaterialCommunityIcons.glyphMap}
+                    size={size}
+                />
+            </View>
+        );
+    }
 }
 
 export default ProfilePicture;
--- src/components/ProfilePicture/styles.tsx
+++ src/components/ProfilePicture/styles.tsx
@@ -1,7 +1,13 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    
+    image:{
+        resizeMode: "cover"
+    },
+    iconContainer:{
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 });
 
 export default styles;
