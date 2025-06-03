6350663b1b048f25e4cb22e6f04287071ce55359
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2025-06-03 12:48:59 -0300

    fix: fixing axios interceptor of auth endpoints

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -3,6 +3,7 @@ import { getToken } from "../storage/token";
 import { API_BASE_URL } from "@env";
 
 const LOGIN_PATH = "/auth/login";
+const REGISTER_PATH = "/auth/register";
 
 const api = axios.create({
     baseURL: API_BASE_URL
@@ -10,11 +11,11 @@ const api = axios.create({
 
 api.interceptors.request.use(
     async(config)=>{
-        if (config.url === LOGIN_PATH) {
-            console.log("AXIOS INTERCEPTOR: Skipping Authorization header for login request.");
+        if (config.url === LOGIN_PATH || config.url === REGISTER_PATH) {
+            console.log("AXIOS INTERCEPTOR: Skipping Authorization header for auth request.");
             return config;
         }
-        
+
         const token = await getToken();
         if(token){
             config.headers.Authorization = `Bearer ${token}`;
